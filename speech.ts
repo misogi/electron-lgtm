f44fab43f952a822b3238fbe86493d3a4a11bc88
misogi <dev@misogi.info>
2015-12-08 09:37:20 +0900

    タイミング調整 表示の

--- speech.ts
+++ speech.ts
@@ -24,25 +24,26 @@ export class Speech {
     const inputBalloon = document.getElementById('input-balloon');
     const outputBalloon = document.getElementById('output-balloon');
     const resultImage = document.getElementById('display');
-    const voicesArea = document.getElementById('voices');
+    const outputArea = document.getElementById('output-area');
 
     this.recognition.onresult = (event: SpeechRecognitionEvent) => {
         const inputText: string = event.results.item(0).item(0).transcript;
         inputBalloon.innerText = inputText;
-
-        this.show(voicesArea);
+        this.show(inputBalloon);
 
         if (inputText.match(/ハッピーグルメ弁当/)) {
           this.show(resultImage);
           resultImage.setAttribute('src', 'img/dondon.jpg');
           this.speech('どんどん？', outputBalloon);
+          this.show(outputArea);
         } else {
           this.hide(resultImage);
           zatsudan.talk(inputText, this.context).on('data', (res) => {
             const json = JSON.parse(res);
             this.context = json.context;
             this.speech(json.utt, outputBalloon);
-          })
+            this.show(outputArea);
+          });
         }
     };
 
@@ -56,25 +57,25 @@ export class Speech {
     };
 
     recordButton.onclick = () => {
-        this.recognition.start();
-        indicator.innerText = 'しゃべってください';
-        recordCircle.classList.add('active');
-        voicesArea.classList.add('collapse');
-        voicesArea.classList.remove('visible');
+      this.hide(inputBalloon);
+      this.hide(outputArea);
+      this.recognition.start();
+      indicator.innerText = 'しゃべってください';
+      recordCircle.classList.add('active');
     };
   }
 
-  private show(elem: HTMLElement) {
+  private show(elem: HTMLElement): void {
     elem.classList.add('visible');
     elem.classList.remove('collapse');
   }
 
-  private hide(elem: HTMLElement) {
+  private hide(elem: HTMLElement): void {
     elem.classList.add('collapse');
     elem.classList.remove('visible');
   }
 
-  private speech(res: string, elem: HTMLElement) {
+  private speech(res: string, elem: HTMLElement): void {
     this.synth.text = res;
     elem.innerText = res;
     speechSynthesis.speak(this.synth);
